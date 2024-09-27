20282a6c795a0663757567489b165047323bb623
Vera Ezeagu <veraezeagu@Veras-MacBook-Pro.local>
2024-09-27 12:05:18 +0100

    sort and map products

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,11 +1,18 @@
-import React, { useContext, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import Title from '../components/Title';
+import ProductItem from '../components/ProductItem'
 
 const Collection = () => {
 
     const {products} = useContext(ShopContext);
     const [showFilter, setShowFilter] = useState(false);
+    const [filterProducts, setFilterProducts] = useState([]);
+
+    useEffect(()=>{
+      setFilterProducts(products);
+    })
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       {/* filter options */}
@@ -99,8 +106,25 @@ const Collection = () => {
 
       {/* right side */}
       <div className='flex-1'>
-        <div className='flex justify-between text-base sm:text-2xl mb-4'>
 
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
+          <Title text1={'ALL'} text2={'COLLECTIONS'}/>
+
+          {/* sort products */}
+          <select className='border-2 border-gray-300 text-sm px-2'>
+            <option value="relevant">Sort by: Relevant</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
+          </select>
+        </div>
+
+        {/* map products */}
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+          {
+            filterProducts.map((item, index)=>(
+              <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+            ))
+          }
         </div>
       </div>
 
