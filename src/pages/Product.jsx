9d32011e762c5ea7f9f1795e7f200b0ab27dc288
Vera Ezeagu <veraezeagu@Veras-MacBook-Pro.local>
2024-09-30 03:31:59 +0100

    product information

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
 
 const Product = () => {
 
   const {productId} = useParams();
-  const {products} = useContext(ShopContext);
+  const {products, currency} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('')
 
@@ -40,7 +41,29 @@ const Product = () => {
             </div>
             <div className='w-full sm:w-[80%]'>
               <img className='w-full h-auto' src={image} alt="" />
+            </div>
+        </div>
 
+        {/* product information */}
+        <div className='flex-1'>
+            <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+            <div className='flex items-center gap-1 mt-2'>
+                <img className="w-3 5" src={assets.star_icon} alt="" />
+                <img className="w-3 5" src={assets.star_icon} alt="" />
+                <img className="w-3 5" src={assets.star_icon} alt="" />
+                <img className="w-3 5" src={assets.star_icon} alt="" />
+                <img className="w-3 5" src={assets.star_dull_icon} alt="" />
+                <p className='pl-2'>(205)</p>
+            </div>
+            <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+            <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+            <div className='flex flex-col my-8'>
+                <p>Select Color</p>
+                <div className='flex gap-2'>
+                  {productData.colors.map((item, index)=>(
+                    <button key={index}>{item}</button>
+                  ))}
+                </div>
             </div>
         </div>
       </div>
