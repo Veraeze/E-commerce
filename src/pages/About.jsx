927f90f188bdb3a7906131e80d87383e5c469bf7
Vera Ezeagu <veraezeagu@Veras-MacBook-Pro.local>
2024-10-04 00:27:59 +0100

    about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Title from '../components/Title'
+import {assets} from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const About = () => {
   return (
     <div>
+
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4'>
+          <p>Welcome to codewithvera, the place to find the best gadgets for every taste and occasion. </p>
+          <p>We aim to offer our customers a variety of the latest trends in technology. We’ve come a long way, so we know exactly which direction to take when supplying you with high quality yet budget-friendly products. We offer all of this while providing excellent customer service and friendly support.</p>
+           <b className='text-gray-800'>Our Mission</b>
+           <p>Our mission is to empower creators and developers by granting them access to high quality gadgets and bridging the gap in the tecnology space.</p>
+        </div>
+      </div>
+
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+      
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>We thoroughly check the quality of our goods, working only with reliable suppliers so that you only receive the best quality product.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gray-600'>We believe shopping is a right, not a luxury, so we strive to deliver the best products at the most affordable prices, and ship them to you regardless of your location.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>Our customers are our top priority and through our products we work hard towards building long-lasting and meaningful relations with them.</p>
+        </div>
+      </div>
+
+      <NewsletterBox/>
       
     </div>
   )
