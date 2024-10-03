ecc9511285ef36d1e38694ffb85b31c723ad9810
Vera Ezeagu <veraezeagu@Veras-MacBook-Pro.local>
2024-10-03 12:58:12 +0100

    orders page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div>
 
-      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-4 text-sm'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-20 text-sm'>
          <div>
             <img className= 'mb-5 w-32' src={assets.logo} alt="" />
             <p className='w-full md:w-2/3 text-gray-600'>
--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
 import { assets } from '../assets/assets'
+import { ShopContext } from '../context/ShopContext'
 
 const PlaceOrder = () => {
+
+  const [method, setMethod] = useState('bt');
+  const {navigate} = useContext(ShopContext);
+
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
       {/* left side */}
@@ -37,18 +42,21 @@ const PlaceOrder = () => {
           <Title text1={'PAYMENT'} text2={'METHOD'}/>
           {/* payment methods */}
           <div className='flex gap-3 flex-col lg:flex-row'>
-            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer' onClick={()=>setMethod('stripe')}>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
               <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
             </div>
-            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer' onClick={()=>setMethod('paystack')}>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'paystack' ? 'bg-green-400' : ''}`}></p>
               <img className='h-5 mx-4' src={assets.paystack_logo} alt="" />
             </div>
-            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer' onClick={()=>setMethod('bt')}>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'bt' ? 'bg-green-400' : ''}`}></p>
               <p className='mx-4 text-gray-500 text-sm font-medium'>BANK TRANSFER</p>
             </div>
+          </div>
+          <div className='w-full text-end mt-8'>
+              <button className='bg-black text-white px-16 py-3 text-sm' onClick={()=>navigate('/orders')}>PLACE ORDER</button>
           </div>
         </div>
       </div>
